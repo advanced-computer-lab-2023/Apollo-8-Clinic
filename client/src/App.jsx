1a255558483f21c46a9d2060990302e228d93f8d
helenmouris <helenmouris3@gmail.com>
2023-10-08 01:57:01 +0300

    add client folder

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Routes></Routes>
+    </div>
+  );
+}
+
+export default App;
