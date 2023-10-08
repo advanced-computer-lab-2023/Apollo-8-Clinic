5ce682d7d83cd64013b7d9f25d3aaa6099c640a0
Nada :) <nada.emadnagy222@gmail.com>
2023-10-08 22:11:41 +0300

    appointment enum

--- server/models/appointment.js
+++ server/models/appointment.js
@@ -17,7 +17,7 @@ const appointmentSchema = new mongoose.Schema({
   },
   status: {
     type: String,
-    enum:["Booked","Free"],
+    enum:["upcoming", "completed", "cancelled", "rescheduled"],
     required: true,
   },
 });
