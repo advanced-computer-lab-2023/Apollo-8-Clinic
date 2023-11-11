af9021769f575572322d24b6daa19974e6821383
youssef Poulis <youssef@youssefs-MacBook-Pro-2.local>
2023-11-11 19:42:26 +0200

    adding the first Landing page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,6 +9,13 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import TextField from '@mui/material/TextField';
 import "../App.css";
+import Button1 from 'react-bootstrap/Button';
+import Card1 from 'react-bootstrap/Card';
+import famImg from '../images/famclinic.png';
+import hpImg from '../images/clinicHP.png';
+import Form from 'react-bootstrap/Form';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Table from 'react-bootstrap/Table';
 
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
@@ -51,6 +58,14 @@ import AddCircleIcon from '@mui/icons-material/AddCircle';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import BottomBar from './BottomBar';
 
+import img1 from '../images/admin.png'
+import img2 from '../images/doc.png'
+import img3 from '../images/pat.png'
+import img4 from '../images/docsign.jpeg'
+import img5 from '../images/docSignUp.jpeg'
+
+
+
 function Home() {
   const navigate = useNavigate();
   const handleDoctorSignup = () => {
@@ -79,50 +94,55 @@ function Home() {
       <AppBar style={{ height: "100%", backgroundColor: "#F0F0F0", overflowY: "auto" }}>
 
         <ResponsiveAppBar />
-        <div className="card m-3 col-12" style={{ width: "80%", borderRadius: '20px', left: '8%' }}>
-          <div className="card-body text-center">
-            <body>
-              <div>
-                <button
-                  className="btn btn-success m-3 btn-lg"
-                  onClick={handleDoctorSignup}
-                >
-                  DoctorSignup
-                </button>
-              </div>
-              <div>
-                <button
-                  className="btn btn-success m-3 btn-lg"
-                  onClick={handlePatientSignup}
-                >
-                  PatientSignup
-                </button>
-              </div>
-              <div>
-                <button
-                  className="btn btn-success m-3 btn-lg"
-                  onClick={handleAdminLogin}
-                >
-                  AdminLogin
-                </button>
-              </div>
-              <div>
-                <button
-                  className="btn btn-success m-3 btn-lg"
-                  onClick={handleDoctorLogin}
-                >
-                  DoctorLogin
-                </button>
-              </div>
-              <div>
-                <button
-                  className="btn btn-success m-3 btn-lg"
-                  onClick={handlePatientLogin}
-                >
-                  PatientLogin
-                </button>
-              </div>
-            </body>
+        <div className="card m-3 col-12" style={{ width: "80%", borderRadius: '20px', left: '8%', display: 'flex' }}>
+          <div style={{ display: 'flex' }}>
+            <Card1 style={{ width: '18rem', marginRight: '10%' }}>
+              <Card1.Img variant="top" src={img3} alt="health package.png" />
+              <Card1.Body>
+                <Card1.Title>Patient Login</Card1.Title>
+
+                <Button1 variant="primary" onClick={handlePatientLogin}>Login</Button1>
+              </Card1.Body>
+            </Card1>
+            <Card1 style={{ width: '18rem', marginRight: '10%' }}>
+              <Card1.Img variant="top" src={img2} alt="family.png" />
+              <Card1.Body>
+                <Card1.Title style={{ marginTop: '45px' }}>Doctor Login</Card1.Title>
+
+                <Button1 variant="primary" style={{}} onClick={handleDoctorLogin}>Login</Button1>
+              </Card1.Body>
+            </Card1>
+            <Card1 style={{ width: '18rem' }}>
+              <Card1.Img variant="top" src={img1} alt="family.png" />
+              <Card1.Body>
+                <Card1.Title>Admin Login</Card1.Title>
+
+                <Button1 variant="primary" onClick={handleAdminLogin}>Login</Button1>
+              </Card1.Body>
+            </Card1>
+
+          </div>
+          <br />
+          <br />
+          <div style={{ display: 'flex' }}>
+            <Card1 style={{ width: '18rem', marginRight: '11%', marginLeft: '18%' }}>
+              <Card1.Img variant="top" src={img4} alt="health package.png" />
+              <Card1.Body>
+                <Card1.Title>Patient SignUp</Card1.Title>
+
+                <Button1 variant="primary" onClick={handlePatientSignup}>Sign Up</Button1>
+              </Card1.Body>
+            </Card1>
+            <Card1 style={{ width: '18rem' }}>
+              <Card1.Img variant="top" src={img5} alt="family.png" />
+              <Card1.Body>
+                <Card1.Title>Doctor SignUp</Card1.Title>
+
+                <Button1 variant="primary" onClick={handleDoctorSignup}>Sign Up</Button1>
+              </Card1.Body>
+            </Card1>
+
+
           </div>
         </div>
         <BottomBar />
