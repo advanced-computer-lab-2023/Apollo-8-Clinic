b1143361e3cbbd90517276f00c777f4717846cf8
MyriamIhab <127784725+MyriamIhab@users.noreply.github.com>
2023-12-03 12:59:49 +0200

    backend requirements for requirements 47-49, 66
    backend requirements for cancelling and rescheduling the appointments for the doctor and the patient

--- server/routes/appointment.js
+++ server/routes/appointment.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 // to test this send a post request to this route: http://localhost:8000/doctor
 router.post("/", Middle.requireAuthPatient, controllers.createAppointment);
+router.get("/", Middle.requireAuthPatient, controllers.getAllAppointments);
 router.get("/:doctorName", Middle.requireAuthDoctor, controllers.getAppointments);
 router.get("/getPatientAppointments/:id", Middle.requireAuthPatient, controllers.getPatientAppointments);
+router.post("/rescheduleAppointment/:id", Middle.requireAuthPatient, controllers.rescheduleAppointment);
+router.post("/cancelAppointment/:id", Middle.requireAuthPatient, controllers.cancelAppointment);
 export default router;
